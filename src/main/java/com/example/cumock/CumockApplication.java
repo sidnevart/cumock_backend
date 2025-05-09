9959f7c5786dc1eacb47c9115d17266212ddfb71
sidnevart <a.sidnevart@gmail.com>
2025-05-09 14:33:46 +0300

    Add Spring Boot + Gradle

---
+++ src/main/java/com/example/cumock/CumockApplication.java
@@ -0,0 +1,13 @@
+package com.example.cumock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CumockApplication.class, args);
+	}
+
+}
