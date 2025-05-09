52302bae186fcc3f030a143e8fa24160a9ce440c
sidnevart <a.sidnevart@gmail.com>
2025-05-09 14:43:46 +0300

    configure data source and add V1__init_users migration

---
+++ src/main/resources/db/migration/V1_init_users.sql
@@ -0,0 +1,6 @@
+-- Создаём базовую таблицу пользователей
+CREATE TABLE users (
+                       id BIGSERIAL PRIMARY KEY,
+                       username VARCHAR(50) NOT NULL UNIQUE,
+                       created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now()
+);
